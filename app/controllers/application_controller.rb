27c427ded54334fe44a3edff06aeebd1964a1e31
April17 <fanmeng0417@gmail.com>
2019-06-05 15:39:17 -0400

    gogogog

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,51 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :meathod_override, true
+  end
+
+  post '/artists' do
+    artist = Artist.create(params)
+    redirect "/artists/#{artist.id}"
   end
 
   get "/" do
+    @artists = Artist.all
     erb :welcome
   end
+
+  get '/artists/new' do
+    erb :new
+  end
+
+  get '/artists/:id' do
+    @artist = Artist.find_by(id: params[:id])
+    @name = @artist.name
+    erb :show
+  end
+
+
+  delete '/artists/:id' do
+    @artist = Artist.all.find_by(id: params[:id])
+    @artist.destroy
+    redirect to "/"
+  end
+
+
+  get '/artists/:id/edit' do
+    @artist = Artist.find_by(id: params[:id])
+    erb :edit
+  end
+
+  patch '/artists/:id' do
+    # binding.pry
+    @artist = Artist.find_by(id: params[:id])
+    @artist.update(params["artist"])
+    redirect "/artists/#{@artist.id}"
+  end
+
+
+
+
 
 end
